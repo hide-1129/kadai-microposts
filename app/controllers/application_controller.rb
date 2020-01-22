4ab5a23a854674aac8bf0afda12925ddfaa6e7c7
hide <katohidetaka1129@yahoo.co.jp>
2020-01-22 14:05:16 +0000

    post

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,5 +8,17 @@ class ApplicationController < ActionController::Base
       unless logged_in?
         redirect_to login_url
       end
+    end
+    
+    private
+    
+    def require_user_logget_in
+      unless logget_in?
+        redirecto_to login_url
+      end 
+    end 
+    
+    def counts(user)
+      @count_microposts = user.microposts.count
     end  
 end
