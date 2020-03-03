ca9cb8598da7c40081bba67c11593cbd4e7ac3c6
hide <katohidetaka1129@yahoo.co.jp>
2020-03-03 09:00:06 +0000

    kadai-microposts

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,13 +5,13 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.favorite(micropost)
     flash[:success] = 'お気に入り登録しました。'
-    redirect_to root_path
+    redirect_to likes_user_path(current_user)
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
-    current_user.unfavorite(micropost)
+    current_user.fav_microposts(micropost)
     flash[:success] = 'お気に入りを削除しました。'
-    redirect_to microposts_url
+    redirect_to micropost_url
   end
 end
